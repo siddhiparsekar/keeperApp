110789e291226c7e527f23086258127a0ae75176
Siddhi <siddhiparsekar06336@gmail.com>
2023-01-02 02:49:37 +0530

    keeper-part1-complete

---
+++ src/components/Note.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Note(){
+    return (
+        <div className="note">
+            <h3>This is the note title</h3>
+            <p>This is the note content</p>
+        </div>
+    )
+}
+export default Note;
